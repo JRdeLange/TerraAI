a3eeb49cf6eb121b20f85833591ff8d650dbab08
KJMH <jobheeres@hotmail.nl>
2023-02-23 22:23:05 +0100

    added neighbour get function

--- territory/territory.py
+++ territory/territory.py
@@ -6,7 +6,23 @@ class Territory:
 
         # variables that will be set when populating the world
         self.owner = None
-        self. neighbours = []
+        self.neighbours = []
 
     def set_neighbours(self, neighbours):
         self.neighbours = neighbours
+
+    def get_neighbour(self, direction):
+        if direction == "north":
+            return self.neighbours[0]
+        elif direction == "north-west":
+            return self.neighbours[1]
+        elif direction == "north-east":
+            return self.neighbours[2]
+        elif direction == "south":
+            return self.neighbours[3]
+        elif direction == "south-west":
+            return self.neighbours[4]
+        elif direction == "south-east":
+            return self.neighbours[5]
+        else:
+            raise Exception("no such direction for territory neighbour")
--- world/world.py
+++ world/world.py
@@ -31,17 +31,29 @@ class World:
         # Get the neighbour above the territory
         if not y == (self.world_size - 1):
             neighbours.append(self.territory_grid[x][y + 1])
+        else:
+            neighbours.append(None)
         # Get the neighbours up and left and up and right of the territory
-        if not x == (self.world_size - 1):
-            neighbours.append(self.territory_grid[x + 1][y])
         if not x == 0:
             neighbours.append(self.territory_grid[x - 1][y])
+        else:
+            neighbours.append(None)
+        if not x == (self.world_size - 1):
+            neighbours.append(self.territory_grid[x + 1][y])
+        else:
+            neighbours.append(None)
         # Get the three neighbours below the territory
-        if not x == (self.world_size - 1) and not y == 0:
-            neighbours.append(self.territory_grid[x + 1][y - 1])
-        if not x == 0 and not y == 0:
-            neighbours.append(self.territory_grid[x - 1][y - 1])
         if not y == 0:
             neighbours.append(self.territory_grid[x][y - 1])
+        else:
+            neighbours.append(None)
+        if not x == 0 and not y == 0:
+            neighbours.append(self.territory_grid[x - 1][y - 1])
+        else:
+            neighbours.append(None)
+        if not x == (self.world_size - 1) and not y == 0:
+            neighbours.append(self.territory_grid[x + 1][y - 1])
+        else:
+            neighbours.append(None)
 
         return neighbours
