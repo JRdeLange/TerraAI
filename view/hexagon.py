125d2c7205facfcb65d538728071309393cd08ad
Jordy de Lange <mrshroom1988@gmail.com>
2023-02-23 22:38:09 +0100

    first version of drawing complete

--- view/hexagon.py
+++ view/hexagon.py
@@ -3,11 +3,17 @@ import math
 
 class Hexagon:
 
-    def __init__(self, size, position, color):
+    def __init__(self, size, territory):
         self.size = size
-        self.position = position
-        self.color = color
+        self.territory = territory
+        self.position = self.calc_position()
         self.vertices = self.calc_vertices()
+
+    def calc_position(self):
+        x = self.territory.x_coordinate
+        y = self.territory.y_coordinate
+        print(x, y)
+        return [x * self.size * 2, y * self.size * 2]
 
     def calc_vertices(self):
         vertices = []
@@ -15,6 +21,13 @@ class Hexagon:
             angle_deg = 60 * i
             angle_rad = angle_deg * (3.14159 / 180)
             x = self.position[0] + self.size * math.cos(angle_rad)
-            y = self.position[0] + self.size * math.sin(angle_rad)
+            y = self.position[1] + self.size * math.sin(angle_rad)
             vertices.append((x, y))
         return vertices
+
+    def get_color(self):
+        #return self.territory.get_color
+        return [255, 255, 255]
+
+    def get_vertices(self):
+        return self.vertices
--- view/view.py
+++ view/view.py
@@ -12,6 +12,10 @@ class View:
         self.width = config.window_width
         self.height = config.window_height
         self.window = pygame.display.set_mode([self.width, self.height])
+        self.hex_size = self.width - config.window_padding * 2
+
+        self.hexagons = []
+        self.initialize_hexagons()
 
     def tick(self):
         self.clear()
@@ -23,14 +27,23 @@ class View:
         self.draw_territories()
 
     def draw_territories(self):
-        hexagon = Hexagon(60, [400, 400], [255, 0, 0])
-
-        self.draw_hexagon(hexagon)
+        for hexagon in self.hexagons:
+            self.draw_hexagon(hexagon)
 
     def draw_hexagon(self, hexagon):
-        pygame.draw.polygon(self.window, hexagon.color, hexagon.vertices)
+        pygame.draw.polygon(self.window, hexagon.get_color(), hexagon.get_vertices())
 
     def clear(self):
-        self.window.fill([255, 255, 255])
+        self.window.fill([130, 130, 130])
+
+    def initialize_hexagons(self):
+        for x in range(config.world_size):
+            for y in range(config.world_size):
+                territory = self.world.get_territory(x, y)
+                hexagon = Hexagon(15, territory)
+                self.hexagons.append(hexagon)
+
+
+
 
 
